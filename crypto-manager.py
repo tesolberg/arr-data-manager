6c4c6659055d90bbe696fc6492122024e51882e7
Tormod Elias Solberg <tesolberg@Tormods-MacBook-Air.local>
2022-01-28 08:47:43 +0100

    .

--- crypto-manager.py
+++ crypto-manager.py
@@ -1,24 +1,45 @@
+from turtle import clear
 import pgpy
-from pgpy.constants import PubKeyAlgorithm, KeyFlags, HashAlgorithm, SymmetricKeyAlgorithm, CompressionAlgorithm
-
-# # we can start by generating a primary key. For this example, we'll use RSA, but it could be DSA or ECDSA as well
-# key = pgpy.PGPKey.new(PubKeyAlgorithm.RSAEncryptOrSign, 4096)
-
-# # we now have some key material, but our new key doesn't have a user ID yet, and therefore is not yet usable!
-# uid = pgpy.PGPUID.new('Abraham Lincoln', comment='Honest Abe', email='abraham.lincoln@whitehouse.gov')
-
-# # now we must add the new user id to the key. We'll need to specify all of our preferences at this point
-# # because PGPy doesn't have any built-in key preference defaults at this time
-# # this example is similar to GnuPG 2.1.x defaults, with no expiration or preferred keyserver
-# key.add_uid(uid, usage={KeyFlags.Sign, KeyFlags.EncryptCommunications, KeyFlags.EncryptStorage},
-#             hashes=[HashAlgorithm.SHA256, HashAlgorithm.SHA384, HashAlgorithm.SHA512, HashAlgorithm.SHA224],
-#             ciphers=[SymmetricKeyAlgorithm.AES256, SymmetricKeyAlgorithm.AES192, SymmetricKeyAlgorithm.AES128],
-#             compression=[CompressionAlgorithm.ZLIB, CompressionAlgorithm.BZ2, CompressionAlgorithm.ZIP, CompressionAlgorithm.Uncompressed])
-
-# print(key)
-# print(key.pubkey)
 
 
-# A key can be loaded from a file, like so:
-imported_key, _ = pgpy.PGPKey.from_file('public-key/test-key.txt')
-# print(imported_key)
+
+def fetch_new_submissions(privateKeyPath, encryptedSubmissionsPath, outputPath):
+
+    privkey = pgpy.PGPKey.from_file(privateKeyPath)
+
+    # iterer over filer i encryptedSubmissionsPath dir
+
+    # les inn den krypterte filen
+    encryptedMsg = pgpy.PGPMessage.from_file("file")
+    # dekrypter innholdet
+    # lagre innholdet i en ny fil i outputPath med samme filnavn
+    # slett den krypterte filen
+
+    return
+
+
+def test():
+    # A key can be loaded from a file, like so:
+    priv_key, _ = pgpy.PGPKey.from_file('public-key/private-key-test.txt')
+    pub_key, _ = pgpy.PGPKey.from_file('public-key/public-key-test.txt')
+
+    # As usual, construct a PGPMessage from a string:
+    message = pgpy.PGPMessage.new("42 is quite a pleasant number")
+
+    # Transform it into a new PGPMessage that contains an encrypted form of the
+    # unencrypted message
+    encrypted_message = pub_key.encrypt(message)
+
+    f = open("public-key/encrypted-test-message.txt", "w")
+    f.write(str(encrypted_message))
+    f.close()
+
+    cryptomsg = pgpy.PGPMessage.from_file("public-key/encrypted-test-message.txt")
+
+    # # after the sender sends the encrypted message, the recipient decrypts:
+    plaintext = priv_key.decrypt(cryptomsg).message
+    print(plaintext)
+
+
+
+test()
